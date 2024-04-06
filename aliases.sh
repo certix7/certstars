2ce0ebcd5c9c24a84fc9b03873c9f6e62173e763
cetrix <30960773+certix7@users.noreply.github.com>
2024-04-06 17:29:27 -0400

    Add files via upload

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+cd /home/cetrix
+sudo nano .zshrc
+
+
+
+
+
+
+
+
+alias h="history"
+alias burb="cd /home/cetrix/Apps/burb ;java -jar burploader.jar" 
+alias st="cd /home/cetrix/Apps/studio/bin ;./studio.sh " 
+alias e="exit"
+alias s="sudo apt install"
+alias ls="lsd"
+
---
+++ practiceVim.sh
@@ -0,0 +1,25 @@
+
+
+ahmed naser mohame 
+A = end of line
+0=start of the line
+$=end of the line
+I = start of line
+g = top of thepage
+G=end of the page
+o=dwon new line
+O=up new line 
+d=delete line
+====================
+w= next word
+b=back word
+x= delete leter
+
+
+
+
+this is line one
+
+welcome 
+
+
