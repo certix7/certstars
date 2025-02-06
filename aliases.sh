2238fb7f016cec02fbbb5210b0384789fe31c8d8
cetrix <30960773+certix7@users.noreply.github.com>
2025-02-06 03:34:17 -0500

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -9,9 +9,10 @@ sudo nano .zshrc
 
 
 alias h="history"
-alias burb="cd /home/cetrix/Apps/burb ;java -jar burploader.jar" 
-alias st="cd /home/cetrix/Apps/studio/bin ;./studio.sh " 
 alias e="exit"
 alias s="sudo apt install"
 alias d="sudo dpkg i "
 alias zap="sudo /opt/lampp/manager-linux-x64.run"
+alias burb="cd /home/cetrix/Apps/burb ;java -jar burploader.jar" 
+alias st="cd /home/cetrix/Apps/studio/bin ;./studio.sh " 
+
